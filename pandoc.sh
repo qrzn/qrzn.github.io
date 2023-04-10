f57113175de9b37ec6ce3c11321e4f5117e4ad26
qrzn <qrzn@riseup.net>
2023-04-10 08:18:44 +0200

    fixed dropdown css

--- pandoc.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for i in $HOME/git/qrzn.github.io/ptry/posts/*.md;
-  do pandoc -f markdown --template $HOME/template.html -s "$i" > "${i%.*}".html; 
-done;
-
-printf "done converting markdown to html in /ptry/posts/"
